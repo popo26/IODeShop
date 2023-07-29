a915b9f8f0adaee6982a6acb16b597000c8545a3
popo26 <mynameispopo26@gmail.com>
2023-07-30 07:48:10 +1200

    3 source files added

---
+++ iodEshop.js
@@ -0,0 +1,7 @@
+  
+    document.getElementById("header").innerHTML = "Hello! Javascript";
+    document.getElementById("aside").style.backgroundColor = "green";
+
+    function orderNow() {
+      alert("Your Order will be shipped!");
+    }
